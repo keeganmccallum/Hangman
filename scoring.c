dc979e9de907430a17ba816d9ca1900c8c6f6de1
SongEric <st.ericsong@yahoo.com>
2015-02-10 18:09:42 -0500

    void scoring(int difficulty, int numGuess)
    purpose: display a message depending on difficulty chosen by the player & the amount of guesses the player took
    
    i.e. if the player guessed the word in 1 try on 'easy' mode, display 'Perfect"

---
+++ scoring.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Only called upon if the user wins
+void score(int difficulty,int numGuess){
+  //numGuesses is the amount of tries the user took to win
+
+  if(difficulty == 1){
+    if(numGuess == 1){
+      printf("Perfect!/n");
+    }else if(numGuess > 1 && numGuess <=3){
+      printf("Well Done\n");
+    }else{
+      printf("You're awful\n");
+    }//end if
+  }else if(difficulty == 2){
+    if(numGuess == 1){
+      printf("Perfect!\n");
+    }else if(numGuess > 1 && numGuess <=3){
+      printf("Well Done\n");
+    }else{
+      printf("You're terrible\n");
+    }//end if
+  }else{
+    if(numGuess == 1){
+      printf("Perfect!\n");
+    }else if(numGuess > 1 && numGuess <=3){
+      printf("Well Done\n");
+    }else{
+      printf("Horrible\n");
+    }//end if
+  }//end if
+
+}//end score
